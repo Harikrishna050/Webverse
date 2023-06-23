04b7d1a43c8da5f40dd4734db7ae6211cd0fb5b5
P HARIKRISHNA <pailiharintr359@gmail.com>
2023-06-24 04:31:32 +0530

    initial commit

---
+++ client/JS/script.js
@@ -0,0 +1,32 @@
+// // var swiper = new Swiper(".course-slider", {
+    
+// //     spaceBetween: 20,
+// //     grabCursor:true,
+// //     pagination: {
+// //       el: ".swiper-pagination",
+// //       clickable:true,
+// //     },
+// //     breakpoints: {
+// //       540: {
+// //         slidesPerView: 2,
+         
+// //       },
+// //       768: {
+// //         slidesPerView: 4,
+         
+// //       },
+// //       1024: {
+// //         slidesPerView: 5,
+         
+// //       },
+// //     },
+// // });
+
+// var swiper = new Swiper(".card-slider", {
+//     slidesPerView: 3,
+//     spaceBetween: 30,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });
