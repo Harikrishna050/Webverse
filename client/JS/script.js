69314374068774eef70c820a67b40e2c6f44e5ce
Hari <pailiharintr359@gmail.com>
2023-08-10 23:43:39 +0530

    Initial commit

--- client/JS/script.js
+++ client/JS/script.js
@@ -1,32 +1,42 @@
-// // var swiper = new Swiper(".course-slider", {
-    
-// //     spaceBetween: 20,
-// //     grabCursor:true,
-// //     pagination: {
-// //       el: ".swiper-pagination",
-// //       clickable:true,
-// //     },
-// //     breakpoints: {
-// //       540: {
-// //         slidesPerView: 2,
-         
-// //       },
-// //       768: {
-// //         slidesPerView: 4,
-         
-// //       },
-// //       1024: {
-// //         slidesPerView: 5,
-         
-// //       },
-// //     },
-// // });
+// student features
+// let feats=document.querySelectorAll(".fa-angle-down");
+// feats.forEach(function(feat){
+//     feat.addEventListener("click",function(event){
+//         event.target.parentElement.nextElementSibling.classList.toggle("active")
+//         event.target.parentElement.parentElement.classList.toggle("active")
+//         event.target.classList.toggle('fa-angle-up');
+//     })
+// })
 
-// var swiper = new Swiper(".card-slider", {
-//     slidesPerView: 3,
-//     spaceBetween: 30,
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//   });
+
+let warden_feats = document.querySelectorAll(".fa-angle-down");
+warden_feats.forEach(function (feat) {
+    feat.addEventListener("click", function (event) {
+        if (event.target.parentElement.nextElementSibling.classList.contains("courseOpt")) {
+            let childrens = event.target.parentElement.nextElementSibling.children;
+            for (let index = 0; index < childrens.length; index++) {
+                childrens[index].classList.toggle("active");
+
+            }
+        }
+        event.target.parentElement.nextElementSibling.classList.toggle("active")
+
+
+        event.target.classList.toggle('fa-angle-up');
+    })
+})
+
+let account=document.querySelector(".fa-user");
+account.addEventListener("click",function(e){
+    document.querySelector(".account").classList.toggle("active");
+})
+
+let notift=document.querySelector(".fa-bell");
+notift.addEventListener("click",function(e){
+    document.querySelector(".notification").classList.toggle("active");
+    console.log("not")
+})
+
+
+
+
